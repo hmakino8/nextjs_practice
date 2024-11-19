188035019ed5f0b8d1c5f8d0d7a8ebb4fcecebf7
Hiroaki Makino <8makino.42tokyo@gmail.com>
2024-11-19 15:47:26 +0900

    add: Layout home-page blog-page contact-page

---
+++ hp/components/Layout.js
@@ -0,0 +1,62 @@
+// 各ページの共通部分をラップする
+
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+
+export const Layout = ({ children, title = "HP by Nextjs" }) => {
+  return (
+    <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 text-sm font-momo">
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <header>
+        <nav className="bg-gray-800 w-screen">
+          <div className="flex items-center pl-8 h-14">
+            <div className="flex space-x-4">
+              <Link
+                href="/"
+                className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded"
+              >
+                Home
+              </Link>
+              <Link
+                href="/blog-page"
+                className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded"
+              >
+                Blog
+              </Link>
+              <Link
+                href="/contact-page"
+                className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded"
+              >
+                Contact
+              </Link>
+            </div>
+          </div>
+        </nav>
+      </header>
+      <main className="flex flex-1 justify-center items-center flex-col w-screen">
+        {children}
+      </main>
+
+      <footer className="w-full h-12 flex justify-center items-center border-t">
+        <Link
+          className="flex items-center"
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{" "}
+          <Image
+            src="/vercel.svg"
+            alt="Vercel Logo"
+            width={72}
+            height={16}
+            className="h-4 ml-2"
+          />
+        </Link>
+      </footer>
+    </div>
+  );
+};
