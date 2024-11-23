28ae23ded21fde2b303473ffe4d24a5a06157596
Hiroaki Makino <8makino.42tokyo@gmail.com>
2024-11-23 19:07:59 +0900

    update

--- my-first-nextjs/backend/rest_api/settings.py
+++ my-first-nextjs/backend/rest_api/settings.py
@@ -109,7 +109,13 @@ if DEBUG:
         }
     }
 else:
-    DATABASES = {"default": dj_database_url.config()}
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+    # DATABASES = {"default": dj_database_url.config()}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
